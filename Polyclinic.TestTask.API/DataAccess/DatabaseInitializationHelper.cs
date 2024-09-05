5160dee3c70cb476787ff601856443bfe72390b5
somov <somov@pulsarm.ru>
2024-09-06 01:23:47 +0300

    Добавил отладочные данные во все таблицы

--- Polyclinic.TestTask.API/DataAccess/DatabaseInitializationHelper.cs
+++ Polyclinic.TestTask.API/DataAccess/DatabaseInitializationHelper.cs
@@ -24,57 +24,182 @@ namespace Polyclinic.TestTask.API.DataAccess
 
             if (databaseAlreadyCreated)
                 return;
-
             // Наполняем отладочными данными.
-            var therapist = new Specialization() { Name = Specialization.THERAPIST_SPECIALIZATION_NAME };
+            dbContext
+                .InitializeCabinets()
+                .InitializeMedicalDistricts()
+                .InitializeSpecializations()
+                .InitializeDoctors()
+                .InitializePatients()
+                ;
+        }
 
+        #region фабрики отладочных данных
+        private static PolyclinicDbContext InitializeCabinets(this PolyclinicDbContext dbContext)
+        {
+            dbContext.Cabinets.AddRange(Enumerable.Range(1, 100).Select(n => new Cabinet() { Id = n }));
+            dbContext.SaveChanges();
+            return dbContext;
+        }
+
+        private static PolyclinicDbContext InitializeMedicalDistricts(this PolyclinicDbContext dbContext)
+        {
+            dbContext.MedicalDistricts.AddRange(Enumerable.Range(1, 4).Select(n => new MedicalDistrict() { Id = n }));
+            dbContext.SaveChanges();
+            return dbContext;
+        }
+
+        private static PolyclinicDbContext InitializeDoctors(this PolyclinicDbContext dbContext)
+        {
             dbContext.Doctors.Add(new()
             {
                 FIO = "Тихонова Софья Кирилловна",
-                Cabinet = new() { Id = 10 },
-                Specialization = therapist,
-                MedicalDistrict = new() { Id = 1 }
+                Cabinet = dbContext.Cabinets.Find(10)!,
+                Specialization = dbContext.Specializations.Find(1)!,
+                MedicalDistrict = dbContext.MedicalDistricts.Find(1)!
             });
 
             dbContext.Doctors.Add(new()
             {
                 FIO = "Родионов Дмитрий Даниилович",
-                Cabinet = new() { Id = 11 },
-                Specialization = therapist,
-                MedicalDistrict = new() { Id = 2 }
+                Cabinet = dbContext.Cabinets.Find(11)!,
+                Specialization = dbContext.Specializations.Find(1)!,
+                MedicalDistrict = dbContext.MedicalDistricts.Find(2)!
             });
 
             dbContext.Doctors.Add(new()
             {
                 FIO = "Еремин Марк Тимофеевич",
-                Cabinet = new() { Id = 12 },
-                Specialization = therapist,
-                MedicalDistrict = new() { Id = 3 }
+                Cabinet = dbContext.Cabinets.Find(12)!,
+                Specialization = dbContext.Specializations.Find(1)!,
+                MedicalDistrict = dbContext.MedicalDistricts.Find(3)!
             });
 
             dbContext.Doctors.Add(new()
             {
                 FIO = "Литвинов Александр Тихонович",
-                Cabinet = new() { Id = 13 },
-                Specialization = therapist,
-                MedicalDistrict = new() { Id = 4 }
+                Cabinet = dbContext.Cabinets.Find(13)!,
+                Specialization = dbContext.Specializations.Find(1)!,
+                MedicalDistrict = dbContext.MedicalDistricts.Find(4)!
             });
 
             dbContext.Doctors.Add(new()
             {
                 FIO = "Гаврилова Юлия Владиславовна",
-                Cabinet = new() { Id = 20 },
-                Specialization = new() { Name = "Хирург" }
+                Cabinet = dbContext.Cabinets.Find(20)!,
+                Specialization = dbContext.Specializations.Find(2)!,
             });
 
             dbContext.Doctors.Add(new()
             {
                 FIO = "Абрамова Елизавета Михайловна",
-                Cabinet = new() { Id = 21 },
-                Specialization = new() { Name = "Офтальмолог" }
+                Cabinet = dbContext.Cabinets.Find(21)!,
+                Specialization = dbContext.Specializations.Find(3)!,
             });
 
-            await dbContext.SaveChangesAsync();
+            dbContext.SaveChanges();
+            return dbContext;
         }
+
+        private static PolyclinicDbContext InitializeSpecializations(this PolyclinicDbContext dbContext)
+        {
+            dbContext.Specializations.Add(new()
+            {
+                Name = Specialization.THERAPIST_SPECIALIZATION_NAME
+            });
+
+            dbContext.Specializations.Add(new()
+            {
+                Name = "Хирург"
+            });
+
+            dbContext.Specializations.Add(new()
+            {
+                Name = "Офтальмолог"
+            });
+
+            dbContext.Specializations.Add(new()
+            {
+                Name = "Кардиолог"
+            });
+
+            dbContext.SaveChanges();
+            return dbContext;
+        }
+
+        private static PolyclinicDbContext InitializePatients(this PolyclinicDbContext dbContext)
+        {
+            var random = new Random();
+
+            dbContext.Patients.Add(new()
+            {
+                FirstName = "Егор",
+                LastName = "Астафьев",
+                Patronymic = "Витальевич",
+                Address = "Россия, г. Челябинск, Школьная ул., д. 4 кв.185",
+                Gender = "Муж",
+                BirthDate = DateTime.Now.AddYears(random.Next(20, 60) * -1),
+                MedicalDistrict = dbContext.MedicalDistricts.Find(random.Next(1, 5))!
+            });
+
+            dbContext.Patients.Add(new()
+            {
+                FirstName = "Иван",
+                LastName = "Ручкин",
+                Patronymic = "Антонович",
+                Address = "Россия, г. Улан-Удэ, Школьный пер., д. 18 кв.168",
+                Gender = "Муж",
+                BirthDate = DateTime.Now.AddYears(random.Next(20, 60) * -1),
+                MedicalDistrict = dbContext.MedicalDistricts.Find(random.Next(1, 5))!
+            });
+
+            dbContext.Patients.Add(new()
+            {
+                FirstName = "Ева",
+                LastName = "Зинченко",
+                Patronymic = "Ивановна",
+                Address = "Россия, г. Йошкар-Ола, Луговая ул., д. 22 кв.132",
+                Gender = "Жен",
+                BirthDate = DateTime.Now.AddYears(random.Next(20, 60) * -1),
+                MedicalDistrict = dbContext.MedicalDistricts.Find(random.Next(1, 5))!
+            });
+
+            dbContext.Patients.Add(new()
+            {
+                FirstName = "Ксения",
+                LastName = "Игнатова",
+                Patronymic = "Данииловна",
+                Address = "Россия, г. Сыктывкар, Школьный пер., д. 22 кв.191",
+                Gender = "Жен",
+                BirthDate = DateTime.Now.AddYears(random.Next(20, 60) * -1),
+                MedicalDistrict = dbContext.MedicalDistricts.Find(random.Next(1, 5))!
+            });
+
+            dbContext.Patients.Add(new()
+            {
+                FirstName = "Серафима",
+                LastName = "Тургенева",
+                Patronymic = "Ефремовна",
+                Address = "Россия, г. Тамбов, Полевая ул., д. 20 кв.203",
+                Gender = "Жен",
+                BirthDate = DateTime.Now.AddYears(random.Next(20, 60) * -1),
+                MedicalDistrict = dbContext.MedicalDistricts.Find(random.Next(1, 5))!
+            });
+
+            dbContext.Patients.Add(new()
+            {
+                FirstName = "Никита",
+                LastName = "Рудаков",
+                Patronymic = "Алексеевич",
+                Address = "Россия, г. Рязань, Шоссейная ул., д. 5 кв.144",
+                Gender = "Муж",
+                BirthDate = DateTime.Now.AddYears(random.Next(20, 60) * -1),
+                MedicalDistrict = dbContext.MedicalDistricts.Find(random.Next(1, 5))!
+            });
+
+            dbContext.SaveChanges();
+            return dbContext;
+        } 
+        #endregion
     }
 }
