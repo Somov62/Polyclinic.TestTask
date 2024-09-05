b5108ce9c208f0c2acf22dd2c5c918de2136097b
Somov62 <2003misha980@gmail.com>
2024-09-06 01:35:04 +0300

    Добавил глобальный обработчик исключений.

---
+++ Polyclinic.TestTask.API/Middlewares/GlobalExceptionHandler.cs
@@ -0,0 +1,38 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
+namespace Polyclinic.TestTask.API.Middlewares;
+
+/// <summary>
+/// Глобальный обработчик исключений.
+/// </summary>
+public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger) : IExceptionHandler
+{
+    private readonly ILogger<GlobalExceptionHandler> _logger = logger;
+
+    /// <summary>
+    /// Обработка исключения, путем возврата пользователю красивого ответа 
+    /// в виде кода ошибки и сообщения с причиной ошибки.
+    /// </summary>
+    public async ValueTask<bool> TryHandleAsync(
+        HttpContext httpContext,
+        Exception exception,
+        CancellationToken cancellationToken)
+    {
+        var exceptionMessage = string.IsNullOrEmpty(exception.InnerException?.Message) ? 
+            exception.Message : 
+            exception.InnerException.Message;
+
+        _logger.LogError(
+            "Error Message: {exceptionMessage}, Time of occurrence {time}",
+            exceptionMessage, DateTime.UtcNow);
+
+        httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+        httpContext.Response.ContentType = "application/text";
+
+        await httpContext.Response.WriteAsync(exceptionMessage, cancellationToken);
+
+        return true;
+    }
+}
--- Polyclinic.TestTask.API/Program.cs
+++ Polyclinic.TestTask.API/Program.cs
@@ -1,4 +1,5 @@
 using Polyclinic.TestTask.API.DataAccess;
+using Polyclinic.TestTask.API.Middlewares;
 using Polyclinic.TestTask.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,12 +7,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddProblemDetails();
 builder.Services.AddScoped<PolyclinicDbContext>();
 builder.Services.AddServices();
 
 var app = builder.Build();
 
+app.UseExceptionHandler();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
